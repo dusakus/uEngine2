c7f2be3b24041e6f7153b8a6d3f21712ef4bf64c
dusakus <dusakus@gmail.com>
2015-03-24 22:13:51 +0100

    [PING]: world writer...

---
+++ uGamePing/src/dcode/games/uEngine2/games/uping/ReallyCrappyWorldConstructionObjectBecauseImLazy.java
@@ -0,0 +1,19 @@
+package dcode.games.uEngine2.games.uping;
+
+import java.awt.*;
+
+/**
+ * Created by dusakus on 24.03.15.
+ */
+public class ReallyCrappyWorldConstructionObjectBecauseImLazy {
+
+	public void writeWorldData(LAYERs_GameScene worldMan) {
+		worldMan.areas[16][16] = worldMan.createArea("str", 16, 16, worldMan);
+		BatMan.areas[16][16] = new BatPack();
+		Bat b = new Bat(80, Bat.FACING_LEFT, 380, 80);
+		b.color = Color.BLUE;
+		BatMan.areas[16][16].addBat(b);
+		BatMan.areas[16][16].addBat(new Bat(80, Bat.FACING_RIGHT, 8, 80));
+	}
+
+}
