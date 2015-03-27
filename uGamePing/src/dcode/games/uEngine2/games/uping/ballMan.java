94868d5973c1e40e14517c3e16acd92ee1fc22c0
dusakus <dusakus@gmail.com>
2015-03-27 11:07:34 +0000

    [PING]: switch to mirror collisions, it always works ]:V

--- uGamePing/src/dcode/games/uEngine2/games/uping/ballMan.java
+++ uGamePing/src/dcode/games/uEngine2/games/uping/ballMan.java
@@ -9,74 +9,85 @@ import java.awt.*;
  */
 public class ballMan extends Sprite {
 
-	float direction = 0.7F;
-	//								    _         _
-	int qdir = 1;     // 1 = |_  , 2 = |   , 3 =   | , 4 =  _|
+    float direction = 0.7F;
+    //								    _         _
 
-	float speed = 2F;
+    boolean mirrorX = false, mirrorY = false;
 
-	float X = 100F, Y = 100F;
+    int qdir = 1;     // 1 = |_  , 2 = |   , 3 =   | , 4 =  _|
 
+    float speed = 2F;
 
-	public void tick() {
-
-		int rec = (int) speed + 1;
-
-		for (int i = 0; i < rec; i++) {
+    float X = 100F, Y = 100F;
+    private int collisionLock = 10;
 
 
-			float shiftXf = (speed / rec * direction), shiftYf = (speed / rec * (1 - direction)), tmpf;
+    public void tick() {
 
-			switch (qdir) {
-				case 1:
-					shiftYf = -shiftYf;
-					break;
-				case 2:
-					tmpf = shiftXf;
-					shiftXf = shiftYf;
-					shiftYf = tmpf;
-					break;
-				case 3:
-					tmpf = shiftYf;
-					shiftYf = shiftXf;
-					shiftXf = -tmpf;
-					break;
-				case 4:
-					shiftXf = -shiftXf;
-					shiftYf = -shiftYf;
-			}
+        int rec = (int) speed + 1;
 
-			float tX = X + shiftXf;
-			float tY = Y + shiftYf;
+        for (int i = 0; i < rec; i++) {
 
 
-			int newX = (int) tX;
-			int newY = (int) tY;
+            float shiftXf = (speed / rec * direction), shiftYf = (speed / rec * (1 - direction)), tmpf;
 
-			if (newX - 5 <= 0) {
-				LStData.currentStatus = 302;
-				LStData.currAreaX--;
-				X = 394;
-				return;
-			} else if (newX + 5 >= 400) {
-				LStData.currentStatus = 302;
-				LStData.currAreaX++;
-				X = 6;
-				return;
-			} else if (newY - 5 <= 0) {
-				LStData.currentStatus = 302;
-				LStData.currAreaY--;
-				Y = 234;
-				return;
-			} else if (newY + 5 >= 240) {
-				LStData.currentStatus = 302;
-				LStData.currAreaY++;
-				Y = 6;
-				return;
-			} else {
+            //use mirrors
+
+            if (mirrorX) shiftXf = -shiftXf;
+            if (mirrorY) shiftYf = -shiftYf;
+
+
+            /*
+            switch (qdir) {
+                case 1:
+                    shiftYf = -shiftYf;
+                    break;
+                case 2:
+                    tmpf = shiftXf;
+                    shiftXf = shiftYf;
+                    shiftYf = tmpf;
+                    break;
+                case 3:
+                    tmpf = shiftYf;
+                    shiftYf = shiftXf;
+                    shiftXf = -tmpf;
+                    break;
+                case 4:
+                    shiftXf = -shiftXf;
+                    shiftYf = -shiftYf;
+            }
+*/
+            float tX = X + shiftXf;
+            float tY = Y + shiftYf;
+
+
+            int newX = (int) tX;
+            int newY = (int) tY;
+
+            if (newX - 5 <= 0) {
+                LStData.currentStatus = 302;
+                LStData.currAreaX--;
+                X = 394;
+                return;
+            } else if (newX + 5 >= 400) {
+                LStData.currentStatus = 302;
+                LStData.currAreaX++;
+                X = 6;
+                return;
+            } else if (newY - 5 <= 0) {
+                LStData.currentStatus = 302;
+                LStData.currAreaY--;
+                Y = 234;
+                return;
+            } else if (newY + 5 >= 240) {
+                LStData.currentStatus = 302;
+                LStData.currAreaY++;
+                Y = 6;
+                return;
+            } else {
 
 				/*if (qdir == 1 && LStData.collisionMap.getRGB(newX, newY - 5) == -1) {
-					qdir = 2;
+                    qdir = 2;
 					direction = -direction;
 				}
 				if (qdir == 4 && LStData.collisionMap.getRGB(newX, newY - 5) == -1) {
@@ -104,107 +115,122 @@ public class ballMan extends Sprite {
 					qdir = 2;
 				}
 				*/
-				boolean col1 = LStData.collisionMap.getRGB(newX, newY - 5) == -1;
-				boolean col2 = LStData.collisionMap.getRGB(newX + 5, newY) == -1;
-				boolean col3 = LStData.collisionMap.getRGB(newX, newY + 5) == -1;
-				boolean col4 = LStData.collisionMap.getRGB(newX - 5, newY) == -1;
+                boolean col1 = LStData.collisionMap.getRGB(newX, newY - 5) == -1;
+                boolean col2 = LStData.collisionMap.getRGB(newX + 5, newY) == -1;
+                boolean col3 = LStData.collisionMap.getRGB(newX, newY + 5) == -1;
+                boolean col4 = LStData.collisionMap.getRGB(newX - 5, newY) == -1;
 
-				switch (qdir) {
-					case 1:
-						if (col1) {
-							direction = 2F - direction;
-						}
-						if (col2) {
-							direction = -2F + direction;
-						}
-						break;
-					case 2:
-						if (col2) {
-							qdir++;
-							direction = (1F - direction);
-						}
-						if (col3) {
-							direction -= 2 * direction;
-						}
-						break;
-					case 3:
-						if (col3) {
-							direction = 2F - direction;
-						}
-						if (col4) {
-							direction = -2F + direction;
-						}
-						break;
-					case 4:
-						if (col4) {
-							direction = 2F - direction;
-						}
-						if (col1) {
-							direction = -2F + direction;
-						}
-						break;
-				}
+                if (col1 || col3) mirrorY = !mirrorY;
+                if (col2 || col4) mirrorX = !mirrorX;
 
-				if (qdir == 1 && LStData.bats.collideAt(newX, newY - 5) >= 0) {
-					qdir = 2;
-					direction = LStData.bats.collideAt(newX, newY - 5) / 100F;
-				} else if (qdir == 4 && LStData.bats.collideAt(newX, newY - 5) >= 0) {
-					qdir = 3;
-					direction = -LStData.bats.collideAt(newX, newY - 5) / 100F;
-				} else if (qdir == 2 && LStData.bats.collideAt(newX, newY + 5) >= 0) {
-					qdir = 1;
-					direction = LStData.bats.collideAt(newX, newY + 5) / 100F;
-				} else if (qdir == 3 && LStData.bats.collideAt(newX, newY + 5) >= 0) {
-					qdir = 4;
-					direction = -LStData.bats.collideAt(newX, newY + 5) / 100F;
-				} else if (qdir == 1 && LStData.bats.collideAt(newX + 5, newY) >= 0) {
-					qdir = 4;
-					direction = (LStData.bats.collideAt(newX + 5, newY) / 100F) + 1;
-				} else if (qdir == 2 && LStData.bats.collideAt(newX + 5, newY) >= 0) {
-					qdir = 3;
-					direction = (-LStData.bats.collideAt(newX + 5, newY) / 100F) + 1;
-				} else if (qdir == 4 && LStData.bats.collideAt(newX - 5, newY) >= 0) {
-					qdir = 1;
-					direction = LStData.bats.collideAt(newX - 5, newY) / 100F;
-				} else if (qdir == 3 && LStData.bats.collideAt(newX - 5, newY) >= 0) {
-					qdir = 2;
-					direction = -LStData.bats.collideAt(newX - 5, newY) / 100F;
 
-				}
-				while (direction >= 1) {
-					direction -= 1;
-					qdir++;
-				}
-				if (qdir > 4) qdir = 1;
-				while (direction < 0) {
-					direction += 1;
-					qdir--;
-				}
-				if (qdir < 1) qdir = 4;
-			}
+                int Bcol1 = LStData.bats.collideAt(newX + 5, newY);   // ->
+                int Bcol2 = LStData.bats.collideAt(newX, newY + 5);   // V
+                int Bcol3 = LStData.bats.collideAt(newX - 5, newY);   // <-
+                int Bcol4 = LStData.bats.collideAt(newX, newY - 5);   // /\
 
-			X = tX;
-			Y = tY;
+                if (collisionLock <= 0) {
+                    if (Bcol1 > 0) {
+                        mirrorX = !mirrorX;
+                        if (Bcol1 < 50) {
+                            //mirrorY = !mirrorY;
+                            //direction = Bcol1 * 0.05F;
+                            collisionLock = 10;
 
-			this.setX(newX);
-			this.setY(newY);
-		}
+                        }
+                        if (Bcol1 > 50) {
+                            //mirrorY = !mirrorY;
+                            //direction = (Bcol1 - 50) * 0.05F;
+                            collisionLock = 10;
 
-	}
+                        }
+                    }
+                    if (Bcol2 > 0) {
+                        mirrorY = !mirrorY;
+                        if (Bcol2 < 50) {
+                            //mirrorX = !mirrorX;
+                            //direction = Bcol2 * 0.05F;
+                            collisionLock = 10;
 
-	@Override
-	public Image getCustomTexture() {
-		return null;
-	}
+                        }
+                        if (Bcol2 > 50) {
+                            //mirrorX = !mirrorX;
+                            //direction = (Bcol2 - 50) * 0.05F;
+                            collisionLock = 10;
 
-	@Override
-	public boolean doCustomRender() {
-		return true;
-	}
+                        }
+                    }
+                    if (Bcol3 > 0) {
+                        mirrorX = !mirrorX;
+                        if (Bcol3 < 50) {
+                            //mirrorY = !mirrorY;
+                            //direction = Bcol3 * 0.05F;
+                            collisionLock = 10;
 
-	@Override
-	public void customRender(Graphics2D G) {
-		G.setColor(Color.WHITE);
-		G.fillRect(x - 5, y - 5, 10, 10);
-	}
+                        }
+                        if (Bcol3 > 50) {
+                            //mirrorY = !mirrorY;
+                            //direction = (Bcol3 - 50) * 0.05F;
+                            collisionLock = 10;
+
+                        }
+                    }
+
+                    if (Bcol4 > 0) {
+                        mirrorY = !mirrorY;
+                        if (Bcol4 < 50) {
+                            //mirrorX = !mirrorX;
+                            //direction = Bcol4 * 0.05F;
+                            collisionLock = 10;
+
+                        }
+                        if (Bcol4 > 50) {
+                            //mirrorX = !mirrorX;
+                            //direction = (Bcol4 - 50) * 0.05F;
+                            collisionLock = 10;
+
+                        }
+                    }
+                } else {
+                    collisionLock--;
+                }
+
+
+                while (direction >= 1) {
+                    direction -= 1;
+                    qdir++;
+                }
+                if (qdir > 4) qdir = 1;
+                while (direction < 0) {
+                    direction += 1;
+                    qdir--;
+                }
+                if (qdir < 1) qdir = 4;
+            }
+
+            X = tX;
+            Y = tY;
+
+            this.setX(newX);
+            this.setY(newY);
+        }
+
+    }
+
+    @Override
+    public Image getCustomTexture() {
+        return null;
+    }
+
+    @Override
+    public boolean doCustomRender() {
+        return true;
+    }
+
+    @Override
+    public void customRender(Graphics2D G) {
+        G.setColor(Color.WHITE);
+        G.fillRect(x - 3, y - 5, 6, 10);
+        G.fillRect(x - 5, y - 3, 10, 6);
+    }
 }
