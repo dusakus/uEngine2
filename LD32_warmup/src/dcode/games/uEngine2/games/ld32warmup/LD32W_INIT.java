2cba4f8a78b9515dfdec852f56a14fafd6e49a2a
dusakus <dusakus@gmail.com>
2015-04-10 20:14:03 +0200

    [LD32W]: INIT

---
+++ LD32_warmup/src/dcode/games/uEngine2/games/ld32warmup/LD32W_INIT.java
@@ -0,0 +1,45 @@
+package dcode.games.uEngine2.games.ld32warmup;
+
+import dcode.games.uEngine2.*;
+
+/**
+ * Created by dusakus on 10.04.15.
+ */
+public class LD32W_INIT {
+    public static void main(String[] args) {
+        uGameSetup gs = new uGameSetup();
+        gs.FPS = 60;
+        gs.TPS_logic = 60;
+        gs.TPS_MSX = 0;
+        gs.TPS_BG = 16;
+
+        gs.debug = true;
+        gs.width = 400;
+        gs.height = 300;
+        gs.scale = 2;
+
+        gs.spriteTableSize = 16;
+        gs.enableSpriteWrappers = true;
+        gs.postProcCount = 0;
+        gs.spriteLayerSize = 16;
+
+
+        gs.safeName = "ld32warmup";
+        gs.screenName = "LD32WUP";
+        gs.windowTitle = " Warmup for Ludum Dare 32 ";
+
+        PuGameBase gb = new PuGameBase();
+
+        gb.setup = gs;
+        gb.initialInputHandler = new InHandler();
+        gb.contentInitializer = new PuGameLoader() {
+            @Override
+            public void loadInitialGameContent() {
+                StData.logicTasks.registerBasic(new MainLogic());
+            }
+        };
+
+        Startup.StartGame(gb);
+    }
+
+}
---
+++ LD32_warmup/src/dcode/games/uEngine2/games/ld32warmup/MainLogic.java
@@ -0,0 +1,23 @@
+package dcode.games.uEngine2.games.ld32warmup;
+
+import dcode.games.uEngine2.LOGIC.ILogicTask;
+
+/**
+ * Created by dusakus on 10.04.15.
+ */
+public class MainLogic implements ILogicTask {
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public boolean doRepeat() {
+        return true;
+    }
+}
