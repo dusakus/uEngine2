67d4af52dae6175752007ddf1cf8fbc05305c2b0
dusakus <dusakus@gmail.com>
2015-04-14 00:00:37 +0200

    [RND]: who needs performance...

--- LD32_warmup/src/dcode/games/uEngine2/games/ld32warmup/render/LAYER_GameSceneFRONT.java
+++ LD32_warmup/src/dcode/games/uEngine2/games/ld32warmup/render/LAYER_GameSceneFRONT.java
@@ -12,24 +12,29 @@ import java.awt.*;
  * Created by dusakus on 11.04.15.
  */
 public class LAYER_GameSceneFRONT implements ILayer {
-    @Override
-    public void draw(Graphics2D G2D) {
-        if(LStData.currentStatus == 101 || LStData.currentStatus > 2000){
-            G2D.drawImage(StData.resources.grf.getPartTexture("RF"+LStData.GL.currentLevel,
-                    numbarTools.clamp(((Player)StData.currentGC.currentSC.sprites[2]).inRoomX - 200, 0, LStData.roomWidth-400),
-                    numbarTools.clamp(((Player)StData.currentGC.currentSC.sprites[2]).inRoomY - 150, 0, LStData.roomHeight-300),
-                    400,300
-            ),0,0,null);
-        }
-    }
+	@Override
+	public void draw(Graphics2D G2D) {
+		try {
+			if (LStData.currentStatus == 101 || LStData.currentStatus > 2000) {
+				G2D.drawImage(StData.resources.grf.getPartTexture("RF" + LStData.GL.currentLevel,
+						numbarTools.clamp(((Player) StData.currentGC.currentSC.sprites[2]).inRoomX - 200, 0, LStData.roomWidth - 400),
+						numbarTools.clamp(((Player) StData.currentGC.currentSC.sprites[2]).inRoomY - 150, 0, LStData.roomHeight - 300),
+						400, 300
+				), 0, 0, null);
+			}
 
-    @Override
-    public boolean removeMe() {
-        return false;
-    }
+		} catch (Exception e) {
 
-    @Override
-    public boolean renderMe() {
-        return true;
-    }
+		}
+	}
+
+	@Override
+	public boolean removeMe() {
+		return false;
+	}
+
+	@Override
+	public boolean renderMe() {
+		return true;
+	}
 }
